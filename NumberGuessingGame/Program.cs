03f8376e36680b519fd2c9a91d2a71cf5238cff8
amman <amman@DESKTOP-VO0EAQH>
2025-02-21 13:23:58 +0000

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool playAgain = true; 
+
+        while (playAgain) 
+        {
+            Random random = new Random(); 
+            int numberToGuess = random.Next(1, 101); 
+            int guess = 0; 
+
+            Console.WriteLine("\nGuess the number (between 1 and 100)!");
+
+            while (guess != numberToGuess) 
+            {
+                Console.Write("Enter your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine()); 
+
+                if (guess < 1 || guess > 100)
+                    Console.WriteLine("Enter a valid number between 1 and 100!");
+                else if (guess < numberToGuess)
+                    Console.WriteLine("Too low! Try again.");
+                else if (guess > numberToGuess)
+                    Console.WriteLine("Too high! Try again.");
+                else
+                    Console.WriteLine("Congratulations! You guessed the number.");
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower(); 
+
+            if (response != "yes")
+            {
+                playAgain = false;
+                Console.WriteLine("Thanks for playing!");
+            }
+        }
+    }
+}
